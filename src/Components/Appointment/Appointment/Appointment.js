5e9db7ae5368e9536793da65e09393f50d58a102
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-04-11 10:36:49 +0200

    done

--- src/Components/Appointment/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment/Appointment.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
 import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
+import BookAppointment from '../BookAppointment/BookAppointment';
 
 const Appointment = () => {
 
@@ -13,6 +14,7 @@ const Appointment = () => {
         <div>
             <Navbar></Navbar>
             <AppointmentHeader handleDateChange={handleDateChange}></AppointmentHeader>
+            <BookAppointment date={selectedDate}></BookAppointment>
             <Footer></Footer>
         </div>
     );
--- src/Components/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Components/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -10,7 +10,7 @@ const AppointmentHeader = ({handleDateChange}) => {
         <main style={{height:'600px'}} className="row d-flex align-items-center">
           <div className="col-md-4 offset-md-1">
             <h1 style={{color:'#3A4256'}}>Appointment</h1>
-                <Calendar
+             <Calendar 
                 onChange={handleDateChange}
                     value={new Date()}
                 />
