dac5171d8d8d9100a1fb00c55b4d72b0e48e7226
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-04-12 06:49:47 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import {
 } from "react-router-dom";
 import Home from './Components/Home/Home/Home';
 import Appointment from './Components/Appointment/Appointment/Appointment';
+import Login from './Components/Login/Login/Login';
+import { createContext, useState } from 'react';
 
-
+export const UserContext = createContext()
 function App() {
+
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
+  <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
    <Router>
      <Switch>
        <Route exact path="/">
@@ -18,8 +24,12 @@ function App() {
        <Route  path="/appointment">
       <Appointment></Appointment>
        </Route>
+       <Route path="/login">
+         <Login></Login>
+       </Route>
      </Switch>
    </Router>
+   </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2S6W2GSlTgTg-furJEOveKFNAwJVy3qM",
+    authDomain: "doctors-portal-a966e.firebaseapp.com",
+    projectId: "doctors-portal-a966e",
+    storageBucket: "doctors-portal-a966e.appspot.com",
+    messagingSenderId: "468414746327",
+    appId: "1:468414746327:web:1b9a26e4d39b5071166a92"
+  };
+  export default firebaseConfig;
